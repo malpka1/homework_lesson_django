ab438e0b1fd763fd1fe6586fb6afba6ee7bf0c0b
malpka1 <veranika.ivanouskaya@gmail.com>
2023-06-21 19:00:33 +0200

    lesson16-20

---
+++ api/views.py
@@ -0,0 +1,32 @@
+import requests
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+
+TODOS = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+
+
+def todos(request):
+    if request.method == 'GET':
+        return render(request, 'todos.html', {'todos': TODOS})
+    elif request.method == 'POST':
+        todo = dict(request.POST)
+        for key in todo:
+            todo[key] = todo[key][0]
+        todo['title'] = todo['title'][0]
+        todo['complete'] = bool(todo['complete'][0])
+        todo['id'] = int(todo['id']) if 'id' in todo else 200
+        todo.append(todo)
+        return render(request, 'todos.html', {'todo': TODOS})
+
+
+def get_todo(request, todo_id: int):
+    global TODOS
+    todo = next((todo for todo in TODOS if todo['id'] == todo_id), None)
+    if todo:
+        if request.method == 'GET':
+            return JsonResponse({'todo': todo})
+        elif request.method == 'DELETE':
+            TODOS = [todo for todo in TODOS if todo['id'] != todo_id]
+            return HttpResponse(status=200)
+    return HttpResponse(status=404)
