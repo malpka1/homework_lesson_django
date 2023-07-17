b860f09214b52b43a1be6ece2ab676a0a6d3dfe3
malpka1 <128837407+malpka1@users.noreply.github.com>
2023-07-17 17:09:54 +0200

    Error

---
+++ todo/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for todo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import debug_toolbar
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/todo/', include('todos.urls.api')),
+    path('todo/', include('todos.urls.todos')),
+    path('api/v2/', include('todos.urls.api_v2')),
+    path('auth/', include('rest_authtoken.urls')),
+   # path('api/token/', TokenObtainPairView(), name='token_obtain_pair'),
+    #path('api/token/refresh', TokenRefreshView(), name='token_refresh'),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+                  ] + urlpatterns
---
+++ todos/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers, generics
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+
+class TodoDetail(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoCreate(generics.CreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoUpdate(generics.UpdateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoDelete(generics.DestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
